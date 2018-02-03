ba7ad2979887e247af5ba8e06039118d777dbbb8
Luke Mwila <outlier.developer@gmail.com>
2018-02-03 10:43:51 +0200

    Initial push

--- src/app.js
+++ src/app.js
@@ -2,14 +2,45 @@ import $ from 'jquery'
 import Rx from 'rxjs/Rx'
 import { getSubscriber } from './utils/getSubscriber'
 /*
-var nums = [33, 45, 23, 12, 72, 4, 5]
+const nums = [33, 45, 23, 12, 72, 4, 5]
 
-var nums$ = Rx.Observable.from(nums)
+// Observable form an array
+const nums$ = Rx.Observable.from(nums)
 
 nums$.subscribe(getSubscriber('nums'))
 
+// Observable from an array of objects
+const users = [
+    {name: 'John Doe', email: 'jdoe@mail.com'},
+    {name: 'Sam Smith', email: 'ssmith@mail.com'},
+    {name: 'Jen Thompson', email: 'jthompson@mail.com'},
+]
+
+const users$ = Rx.Observable.from(users)
+
+users$.subscribe(getSubscriber('users'))
+
+// Observable from Set
+
+const set = new Set(['Foo', 43, {name: 'Jeff'}])
+
+const set$ = Rx.Observable.from(set)
+
+set$.subscribe(getSubscriber('set'))
+
+// Observable from Map
+
+const m = new Map([[1,2], [3,4], [5,6]])
+
+const m$ = Rx.Observable.from(m)
+
+m$.subscribe(getSubscriber('map'))
+
 */
 
-ver users = [
-    {name: 'John Doe', email: 'jdoe@mail.com'}
-]
+// Observable from String
+
+const str = 'Hello World'
+const str$ = Rx.Observable.from(str)
+
+str$.subscribe(getSubscriber('str'))
