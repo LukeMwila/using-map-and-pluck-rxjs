058a4e843939503435a3389f901bc391d0f6c6b0
Luke Mwila <outlier.developer@gmail.com>
2018-02-03 12:30:24 +0200

    Initial push

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -82,15 +82,40 @@ var _getSubscriber = __webpack_require__(458);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var source$ = new _Rx2.default.Observable(function (observer) {
-    console.log('creating observable...');
+/*
+    A cold observable is also known as a passive observable, this means the underlying producer is created and activated during the subscription.
+    The producer is the source of the stream of values for the observable. So that could be a DOM event, an iterator or a web socket. Anything you're
+    using to get from observer.next()
+    So when you subscribe, you actually activate it. 
+    An example of a cold observable would be if we set up a request to an API to bring in an array. 
 
-    observer.next('A value');
+    A hot (or active) observable is always activated and running even outside of that observable. A good example of a hot observable is mousemove events, these are always
+    happening whether you subscribe to it or not. If we want to subscribe to that stream, we just tap into it and we can see the values of that stream.
+    To turn a cold observable into a hot observable we make use of the publish() function. That makes it a connectable observable, so we have to connect to it to obtain the values from the stream.
+*/
 
-    observer.complete();
-});
+/*
+const source$ = Rx.Observable.create(observer => {
+    observer.next(Date.now())
+}).publish()
 
-source$.subscribe((0, _getSubscriber.getSubscriber)('myobs'));
+source$.subscribe(getSubscriber('one'))
+source$.subscribe(getSubscriber('two'))
+
+source$.connect()
+
+*/
+
+var source$ = _Rx2.default.Observable.interval(1000).publish();
+
+source$.connect();
+
+setTimeout(function () {
+    source$.subscribe((0, _getSubscriber.getSubscriber)('one'));
+    setTimeout(function () {
+        source$.subscribe((0, _getSubscriber.getSubscriber)('two'));
+    }, 4000);
+}, 2000);
 
 /***/ }),
 /* 1 */
--- src/app.js
+++ src/app.js
@@ -2,12 +2,39 @@ import $ from 'jquery'
 import Rx from 'rxjs/Rx'
 import { getSubscriber } from './utils/getSubscriber'
 
-const source$ = new Rx.Observable(observer => {
-    console.log('creating observable...')
+/*
+    A cold observable is also known as a passive observable, this means the underlying producer is created and activated during the subscription.
+    The producer is the source of the stream of values for the observable. So that could be a DOM event, an iterator or a web socket. Anything you're
+    using to get from observer.next()
+    So when you subscribe, you actually activate it. 
+    An example of a cold observable would be if we set up a request to an API to bring in an array. 
 
-    observer.next('A value')
+    A hot (or active) observable is always activated and running even outside of that observable. A good example of a hot observable is mousemove events, these are always
+    happening whether you subscribe to it or not. If we want to subscribe to that stream, we just tap into it and we can see the values of that stream.
+    To turn a cold observable into a hot observable we make use of the publish() function. That makes it a connectable observable, so we have to connect to it to obtain the values from the stream.
+*/
 
-    observer.complete()
-})
+/*
+const source$ = Rx.Observable.create(observer => {
+    observer.next(Date.now())
+}).publish()
 
-source$.subscribe(getSubscriber('myobs'))
+source$.subscribe(getSubscriber('one'))
+source$.subscribe(getSubscriber('two'))
+
+source$.connect()
+
+*/
+
+const source$ = Rx.Observable
+                    .interval(1000)
+                    .publish()
+
+source$.connect()
+
+setTimeout(() => {
+    source$.subscribe(getSubscriber('one'))
+    setTimeout(() => {
+        source$.subscribe(getSubscriber('two'))
+    }, 4000)
+},2000)
