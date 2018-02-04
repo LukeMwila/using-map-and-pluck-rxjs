83f90533647c549910bd8fd10df6977f1481f41d
Luke Mwila <outlier.developer@gmail.com>
2018-02-04 23:40:10 +0200

    Initial push

--- src/app.js
+++ src/app.js
@@ -2,54 +2,21 @@ import $ from 'jquery'
 import Rx from 'rxjs/Rx'
 import { getSubscriber } from './utils/getSubscriber'
 
-/**
- * Creating observables from a Promise:
- * A Promise is an object used fo asynchronours computations. It can represent a value that can be available now or later on.
- */
-
 /*
- // Promise
- const myPromise = new Promise((resolve, reject) => {
-     console.log('creating promise')
-     setTimeout(() => {
-         console.log('Something')
-         resolve('Hello From Promise!')
-     }, 2000)
- })
+Rx.Observable.interval(1000)
+            .take(10)
+            .map(v => v * v)
+            .subscribe(getSubscriber('map'))
 
- //Without observable
- myPromise.then(x => {
-     console.log(x)
- })
-
-Rx.Observable.fromPromise(myPromise)
-        .subscribe(getSubscriber('promise'))
+            */
+/*
+let names = ['Bob', 'Jane', 'Mike']
+Rx.Observable.from(names)
+            .map(v => v.toUpperCase())
+            .subscribe(getSubscriber('name'))
 */
-let input = $('#input')
-let profile = $('#profile')
-let client_id = ''
-let client_secret = ''
-profile.hide()
 
-Rx.Observable.fromEvent(input, 'keyup')
-            .subscribe(e => {
-                profile.show()
-                Rx.Observable.fromPromise(getGithubUser(e.target.value))
-                  .subscribe(user => {
-                    $('#name').text(user.data.name)
-                    $('#login').text(user.data.login)
-                    $('#blog').text(user.data.blog)
-                    $('#avatar').attr('src', user.data.avatar_url)
-                    $('#repos').text(user.data.public_repos)
-                    $('#followers').text(user.data.followers)
-                    $('#following').text(user.data.following)
-                    $('#link').attr('href', user.data.html_url)
-                  })
-            })
-
-function getGithubUser(username){
-    return $.ajax({
-        url: 'https://api.github.com/users/'+ username +'?client_id='`${client_id}`'&client_secret='`${client_secret}`,
-        dataType: 'jsonp'
-    }).promise()
-}
+const input = $('#input')
+Rx.Observable.fromEvent(input, 'keyup').subscribe(x => {
+    console.log(x)
+})
